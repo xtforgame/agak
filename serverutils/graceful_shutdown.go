97ce8e4c5a12800083c087c091880073773b655e
RickChen <xtforgame@gmail.com>
2022-02-05 03:16:29 +0800

    release for v0.2.0

--- serverutils/graceful_shutdown.go
+++ serverutils/graceful_shutdown.go
@@ -3,6 +3,7 @@ package serverutils
 import (
 	"fmt"
 	"github.com/xtforgame/agak/utils"
+	"net"
 	"net/http"
 	"time"
 )
@@ -11,6 +12,36 @@ func RunAndWaitGracefulShutdown(server *http.Server) {
 	chBack := make(chan error)
 	go func() {
 		_ = server.ListenAndServe()
+		fmt.Println("server shutdown")
+		time.Sleep(time.Second * 1)
+		fmt.Println("server shutdown delay ended")
+		chBack <- nil
+	}()
+
+	utils.HandleSIGINTandSIGTERM()
+
+	// Stop the service gracefully.
+	fmt.Println("start shutdown")
+	fmt.Println("shutdown result :", server.Shutdown(nil))
+	fmt.Println("shutdown called")
+
+	fmt.Println("error :", <-chBack)
+	fmt.Println("exit")
+}
+
+func ListenAndServeUnixSoclet(srv *http.Server, filepath string) error {
+	ln, err := net.Listen("unix", filepath)
+	if err != nil {
+		fmt.Println("err :", err)
+		return err
+	}
+	return srv.Serve(ln)
+}
+
+func RunAndWaitGracefulShutdownUnixSoclet(server *http.Server, filepath string) {
+	chBack := make(chan error)
+	go func() {
+		_ = ListenAndServeUnixSoclet(server, filepath)
 		fmt.Println("server shutdown")
 		time.Sleep(time.Second * 1)
 		fmt.Println("server shutdown delay ended")
